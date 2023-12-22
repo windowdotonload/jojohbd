8a37e850b15daa5be02cf6308d9db9bbfba9387e
rui.chen <rui.chen@inossem.com>
2023-12-22 15:44:31 +0800

    modify

--- src/components/hbd.vue
+++ src/components/hbd.vue
@@ -3,41 +3,60 @@ import { ref } from "vue";
 const playVideo = ref(false);
 const videoRef = ref(null);
 const canPlay = ref(false);
+const imgIndex = ref(2);
 const playAction = () => {
   if (!canPlay.value) return;
   playVideo.value = true;
   videoRef.value.play();
 };
-const canplaythrough = (e) => {
-  canPlay.value = true;
-};
+setInterval(() => {
+  if (imgIndex.value === 3) {
+    imgIndex.value = 1;
+  } else {
+    imgIndex.value++;
+  }
+}, 2800);
 setTimeout(() => {
   canPlay.value = true;
-  alert("alert");
-}, 1000);
+}, 1800);
 </script>
 
 <template>
   <div class="container">
     <div class="content__box">
-      <div
-        style="
-          background: red;
-          width: 100%;
-          height: 100%;
-          z-index: 99;
-          position: absolute;
-        "
-        v-show="!playVideo"
-        @click="playAction"
-      ></div>
+      <div class="hbd__pleaceholder" v-show="!playVideo" @click="playAction">
+        <img
+          :src="`/src/assets/hbd${imgIndex}.jpg`"
+          alt="hbd"
+          class="img_box"
+          :style="{
+            opacity: imgIndex == 1 ? 1 : 0,
+          }"
+        />
+        <img
+          :src="`/src/assets/hbd${imgIndex}.jpg`"
+          alt="hbd"
+          class="img_box"
+          :style="{
+            opacity: imgIndex == 2 ? 1 : 0,
+          }"
+        />
+        <img
+          :src="`/src/assets/hbd${imgIndex}.jpg`"
+          alt="hbd"
+          class="img_box"
+          :style="{
+            opacity: imgIndex == 3 ? 1 : 0,
+          }"
+        />
+      </div>
       <video
+        :style="{ opacity: playVideo ? 1 : 0 }"
         src="../assets/hbd.mp4"
         class="hbd-video"
         loop
         ref="videoRef"
         preload="auto"
-        @canplaythrough="canplaythrough"
         :controls="false"
       />
     </div>
@@ -72,6 +91,23 @@ setTimeout(() => {
   align-items: center;
   justify-content: center;
   overflow: hidden;
+  margin-top: -10%;
+}
+.hbd__pleaceholder {
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  position: absolute;
+}
+.img_box {
+  width: 100%;
+  height: 100%;
+  transition: all 0.8s ease-in-out;
+  position: absolute;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
 }
 .hbd-video {
   height: 100%;
